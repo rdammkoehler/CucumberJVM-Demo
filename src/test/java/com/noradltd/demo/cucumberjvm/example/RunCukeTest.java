cfa367f04ff13d0599380823ae3360780302b75f
Rich Dammkoehler <rpd@noradltd.com>
2013-10-23 11:52:25 -0500

    set output to monochrome, took forever to find that trick

--- src/test/java/com/noradltd/demo/cucumberjvm/example/RunCukeTest.java
+++ src/test/java/com/noradltd/demo/cucumberjvm/example/RunCukeTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = { "pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json" })
+@Cucumber.Options(format = { "pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json" }, monochrome = true) //<< monochrome so it's pretty in Eclipse
 public class RunCukeTest {
 
 }
