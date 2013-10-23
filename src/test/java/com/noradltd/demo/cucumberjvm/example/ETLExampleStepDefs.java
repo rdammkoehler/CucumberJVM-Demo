7f0e230e330671eba778dcca89769720e745aecd
Rich Dammkoehler <rpd@noradltd.com>
2013-10-23 16:14:53 -0500

    added default step defs for ETL feature

---
+++ src/test/java/com/noradltd/demo/cucumberjvm/example/ETLExampleStepDefs.java
@@ -0,0 +1,57 @@
+package com.noradltd.demo.cucumberjvm.example;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.runtime.PendingException;
+
+public class ETLExampleStepDefs {
+
+	@Given("^a nightly orders load file$")
+	public void a_nightly_orders_load_file() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^the file arrives on the landing area$")
+	public void the_file_arrives_on_the_landing_area() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^Nightly Orders are loaded into the ODS$")
+	public void Nightly_Orders_are_loaded_into_the_ODS() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Given("^an empty nightly orders load file$")
+	public void an_empty_nightly_orders_load_file() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^no changes occur in the ODS$")
+	public void no_changes_occur_in_the_ODS() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^an empty file notification is logged$")
+	public void an_empty_file_notification_is_logged() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Given("^a corrupt nightly orders load file$")
+	public void a_corrupt_nightly_orders_load_file() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^a corrupt file notification is logged$")
+	public void a_corrupt_file_notification_is_logged() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+}
