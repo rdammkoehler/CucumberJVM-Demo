1b88b700a71404cf4de7bc1b1bd5f6e6d547b92a
Rich Dammkoehler <rpd@noradltd.com>
2013-10-23 20:07:46 -0500

    many changes, bad programmer bad

--- src/main/java/com/noradltd/demo/cucumberjvm/example/OrdersODS.java
+++ src/main/java/com/noradltd/demo/cucumberjvm/example/OrdersODS.java
@@ -2,11 +2,35 @@ package com.noradltd.demo.cucumberjvm.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
 
 public class OrdersODS {
 
+	private List<Order> orders = new ArrayList<Order>();
+	private Stack<String> log = new Stack<String>();
+
 	public List<Order> find(Order order) {
-		return new ArrayList<Order>();
+		return Lists.newArrayList(Collections2.filter(orders, Predicates.equalTo(order)));
+	}
+
+	public boolean add(Order order) {
+		return orders.add(order);
+	}
+
+	public Integer size() {
+		return orders.size();
+	}
+
+	public Stack<String> log() {	
+		return log;
+	}
+	
+	public void log(String message) {
+		log.push(message);
 	}
 
 }
--- src/test/java/com/noradltd/demo/cucumberjvm/example/ETLExampleStepDefs.java
+++ src/test/java/com/noradltd/demo/cucumberjvm/example/ETLExampleStepDefs.java
@@ -3,9 +3,8 @@ package com.noradltd.demo.cucumberjvm.example;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.io.FileReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,6 +12,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -25,12 +26,36 @@ public class ETLExampleStepDefs {
 	private List<Order> orders = new ArrayList<Order>();
 	private OutputStream ordersOutputStream = new ByteArrayOutputStream();
 	private OrdersODS ordersODS = new OrdersODS();
+	private ETLBogon etlBogon = null;
 
+	@Before
+	public void beforeETLExampleFeature() {
+		if ( etlBogon == null ) {
+			etlBogon = new ETLBogon(ordersODS);
+			etlBogon.start();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@After
+	public void afterELTExampleFeature() {
+		if ( etlBogon != null ) {
+			try {
+				etlBogon.quit().join(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			etlBogon = null;
+		}
+		new File(ORDERS_CSV).delete();
+	}
+	
 	@Given("^a nightly orders load file$")
 	public void a_nightly_orders_load_file() throws Throwable {
-		/*
-		 * Orders File Structure Order#, Order Date (YYYY-MM-DD)
-		 */
 		String[][] ordersData = new String[][] { { "1", "2013-10-23" }, { "2", "2013-10-23" } };
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		for (String[] orderStrings : ordersData) {
@@ -51,6 +76,10 @@ public class ETLExampleStepDefs {
 		} finally {
 			writer.close();
 		}
+		writer = null;
+		synchronized (etlBogon) {			
+			etlBogon.wait(20000);
+		}
 	}
 
 	@Then("^Nightly Orders are loaded into the ODS$")
@@ -69,25 +98,21 @@ public class ETLExampleStepDefs {
 
 	@Then("^no changes occur in the ODS$")
 	public void no_changes_occur_in_the_ODS() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		throw new PendingException();
+		assertThat(ordersODS.size(), is(0)); //not really valid, work it out
 	}
 
 	@Then("^an empty file notification is logged$")
 	public void an_empty_file_notification_is_logged() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		throw new PendingException();
+		assertThat(ordersODS.log().pop(), is("Empty File"));
 	}
 
 	@Given("^a corrupt nightly orders load file$")
 	public void a_corrupt_nightly_orders_load_file() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		throw new PendingException();
+		ordersOutputStream.write("This file is invalid".getBytes());
 	}
 
 	@Then("^a corrupt file notification is logged$")
 	public void a_corrupt_file_notification_is_logged() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		throw new PendingException();
+		assertThat(ordersODS.log().pop(), is("Corrupt Input File"));
 	}
 }
